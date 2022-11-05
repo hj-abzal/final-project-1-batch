539611a4dc42c721b6b2678b51d51b414f29e986
abzal <abzal008@mail.ru>
2022-11-05 09:48:28 +0600

    start of changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
-
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Login} from "./pages/Login/Login";
+import {Register} from "./pages/Register/Register";
+import {PageNotFound} from "./pages/PageNotFound/PageNotFound";
 function App() {
 
   return (
     <div className="App">
-        final project
+      <Routes>
+        <Route path={'/'} element={<Navigate to={'login'}/>}/>
+        <Route path={'/login'} element={<Login/>}/>
+        <Route path={'/register'} element={<Register/>}/>
+        <Route path={'*'} element={<PageNotFound/>}/>
+      </Routes>
     </div>
   )
 }
+
 
 export default App
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,7 @@
+import s from './Login.module.css';
+
+export const Login = () => {
+    return <div className={s.wrapper}>
+        <div className={s.modal}></div>
+    </div>
+}
