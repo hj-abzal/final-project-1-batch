29e692a8b62825ac98e3c681d5482bd3b39e808c
Juliya <jgorelova@mail.ru>
2022-11-05 11:12:49 +0600

    added regisrter page

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,30 @@
+import s from './Register.module.css'
+import {useState} from "react";
+
 export const Register = () => {
-    return <div>Register</div>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const onClickRegistr = () => {
+        alert(email)
+        alert(password)
+        alert(confirmPassword)
+    }
+
+    return <div className={s.wrapper}>
+        <div className={s.modal}>
+            <h2>IT-CARATE</h2>
+            <h3>sign-up </h3>
+            <input placeholder={"EMAIL"} onChange={(e) => setEmail(e.currentTarget.value)} value={email}/>
+            <p></p>
+            <input type="password" onChange={(e) => setPassword(e.currentTarget.value)} value={password}
+                   placeholder={"Password"}/>
+            <input type="password" onChange={(e) => setConfirmPassword(e.currentTarget.value)} value={confirmPassword}
+                   placeholder={"Confirm Password"}/>
+            <button >CANCEL</button>
+            <button onClick={onClickRegistr}>REGISTER</button>
+
+        </div>
+    </div>
 }
